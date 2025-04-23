ce106c7de83d2ae8842a3ac4d8c59b997db6538a
André Victor Soares Castro <95365578+AndreVsc@users.noreply.github.com>
2025-04-23 10:56:39 -0300

    feat: update bottom bar

---
+++ flutter_project_nft/README.md
@@ -0,0 +1,3 @@
+# flutter_project_nft
+
+A new Flutter project.
---
+++ flutter_project_nft/android/app/src/main/kotlin/com/example/flutter_project_nft/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_project_nft
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
